16ef7c053907cd212c2489a911ab977cbb4b216e
Oakville616 <liderrick616@gmail.com>
2023-11-21 03:29:44 -0500

    Completed user story 1
    Successfully implemented login, logout for users(both students and admin), realtime database, data storage

--- app/src/main/java/com/example/cscb07project/Admin_dashboardActivity.java
+++ app/src/main/java/com/example/cscb07project/Admin_dashboardActivity.java
@@ -2,7 +2,9 @@ package com.example.cscb07project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Admin_dashboardActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class Admin_dashboardActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_dashboard_fragment);
-    }
-}
+
+        //click log out
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to login fragment
+                Intent intent = new Intent(Admin_dashboardActivity.this, AdminLoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }}
